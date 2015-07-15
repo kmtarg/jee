581283e6842a40d4814db0b7499a3f2e030ad875
kmtarg <kmtarg@gmail.com>
2015-07-15 22:20:26 +0200

    post method

---
+++ ServetsTesty/src/data/User.java
@@ -0,0 +1,47 @@
+package data;
+
+public class User {
+
+	private String imie;
+	private String nazwisko;
+	private String password;
+	private String[] hobby;
+	private String plec;
+	public String getImie() {
+		return imie;
+	}
+	public void setImie(String imie) {
+		this.imie = imie;
+	}
+	public String getNazwisko() {
+		return nazwisko;
+	}
+	public void setNazwisko(String nazwisko) {
+		this.nazwisko = nazwisko;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String[] getHobby() {
+		return hobby;
+	}
+	public void setHobby(String[] hobby) {
+		this.hobby = hobby;
+	}
+	public String getPlec() {
+		return plec;
+	}
+	public void setPlec(String plec) {
+		this.plec = plec;
+	}
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}
---
+++ ServetsTesty/src/servlets/FormularzStartowy.java
@@ -0,0 +1,88 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import data.User;
+
+/**
+ * Servlet implementation class FormularzStartowy
+ */
+@WebServlet("/FormularzStartowy")
+public class FormularzStartowy extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FormularzStartowy() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		User urzytkownik = generujUsera(request);
+		
+		pokazUsera(response, urzytkownik);
+		
+		
+	}
+
+
+	private void pokazUsera(HttpServletResponse response, User urzytkownik) throws IOException {
+		response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<body>");
+        writer.print("<h2>Dane odebrano pomyœlnie</h2>");
+        writer.print("<div>");
+        writer.println(urzytkownik.getImie() + "<br>");
+        writer.println(urzytkownik.getNazwisko() + "<br>");
+        writer.println(urzytkownik.getPassword() + "<br>");
+        writer.println(urzytkownik.getPlec() + "<br>");
+        if(urzytkownik.getHobby() != null) {
+            writer.print("Hobby: <br>");
+            for(String hobby: urzytkownik.getHobby())
+                writer.println(" " + hobby + "<br>");
+        }
+        writer.print("</div>");
+        writer.println("</body>");
+        writer.println("</html>");
+	}
+
+
+	protected void doPost(HttpServletRequest request1, HttpServletResponse response1) throws ServletException, IOException {
+
+		User urzytkownik = generujUsera(request1);
+		
+		pokazUsera(response1, urzytkownik);
+		
+		
+	}
+	
+	private User generujUsera(HttpServletRequest request){
+		
+		User user = new User();
+		user.setImie(request.getParameter("imie"));
+		user.setNazwisko(request.getParameter("nazwisko"));
+		user.setPassword(request.getParameter("haslo"));
+		user.setPlec(request.getParameter("plec"));
+		user.setHobby(request.getParameterValues("hobby"));
+		
+		return user;
+		
+		
+	}
+	
+	
+
+}
